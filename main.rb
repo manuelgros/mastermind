6b834568a4b3be0f484555b7aaeb7195c398a2f1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-12-13 16:31:33 +0100

    Code class created
    The code that the player will have to crack is selected from an
    array and the permutation method.

--- main.rb
+++ main.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+# Class to create a code (currently: randomly)
 class Code
+  attr_reader :possible_codes
 
-  # code
+  def initialize
+    @possible_codes = Array(1..9).permutation(4).to_a
+  end
 
+  def select_code(database_array)
+    database_array.sample
+  end
 end
