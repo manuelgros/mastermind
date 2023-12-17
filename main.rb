0dbd2c9a4188ec98fd0417e25e64ab745d01546c
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-12-17 11:14:05 +0100

    Included some personal notes

--- main.rb
+++ main.rb
@@ -36,6 +36,17 @@ class Game
     if guessed_code.length != select_code.length
       puts 'Please make sure that you only type in 4 number between 1 and 9:'
       guessed_code = gets.chomp.to_s.split('')
+    else 
+      # code
     end
   end
+
+  def check_for_hit(num)
+    # for loop to check each position against each other for direct hit
+  end
+
+  def check_for_included
+  # Method (.each ?) to check if value from guess_code is included in selected_code
+  end
+
 end
