d922d4fa5cdc16ef7d7757a3e30990a04e069ce1
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-05-28 08:49:08 -0700

    Debugging

--- main.rb
+++ main.rb
@@ -86,7 +86,7 @@ class Game
     text_type_guess
     begin
       @player_guess = gets.chomp.to_s.split('')
-      raise if @player_guess.length != 4 || @player_guess.all?(Integer)
+      raise if @player_guess.length != 4 || !@player_guess.all?(Integer)
     rescue
       text_wrong_code
       retry
