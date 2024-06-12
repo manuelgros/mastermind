07b12f736e5f1e7a4a439f0a7a414f7f9774ab11
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-12 16:15:00 -0700

    Colored description text

--- lib/game_description.rb
+++ lib/game_description.rb
@@ -6,14 +6,14 @@ require 'colorize'
 class GameDescription
   def self.game_description
     puts "WELCOME TO MASTERMIND. Mastermind is a game of skill, in which two players engage in a battle of wits and
-    logic. In this version you will compete with a computer opponent. One will take on the role of CODEMAKER, who 
+    logic. In this version you will compete with a computer opponent. One will take on the role of CODEMAKER, who
     creates a secret code, which the other player, who is called the CODEBREAKER, must attempt to discover.
     The CODEMAKER generates a code which is made up of four numbers, ranging from 1 to 9
     (numbers can be used multiple times). The CODEBREAKER then tries to
-    guess the code and receives a hint after each attempt:
+    guess the code and receives a hint after each attempt:\n".colorize(:blue)
 
-    🟢 if the number at is at its correct position.
-    🟡 if the number is indeed in the code BUT not at this position
-    🔴 if the number is not in the code.\n\n".colorize(:light_yellow)
+    puts "🟢 #{'=> if the number at is at its correct position'.colorize(:green)}
+🟡 #{'=> if the number is in code BUT not at this position'.colorize(:yellow)}
+🔴 #{'=> if the number is not in the code'.colorize(:red)}\n\n"
   end
 end
