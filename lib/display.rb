711d43a3f4d1c66fc2ea3be52f4b5387cd7ca9c7
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-12 16:03:03 -0700

    Created module for more esthetic interface display

---
+++ lib/display.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# module to display game content more visually pleasing
+module Display
+  def code_colors(number)
+    {
+      '1' => "\e[101m  1  \e[0m ",
+      '2' => "\e[43m  2  \e[0m ",
+      '3' => "\e[44m  3  \e[0m ",
+      '4' => "\e[45m  4  \e[0m ",
+      '5' => "\e[46m  5  \e[0m ",
+      '6' => "\e[41m  6  \e[0m "
+    }[number]
+  end
+
+  def show_code(array)
+    array.each { |num| print code_colors(num) }
+  end
+
+  def clue_colors(clue)
+    {
+      '!' => '🟢 ',
+      '?' => '🟡 ',
+      'x' => '🔴 '
+    }[clue]
+  end
+
+  def show_clue(array)
+    array.each { |sym| print clue_colors(sym) }
+  end
+end
