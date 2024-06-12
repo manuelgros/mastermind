93db7bf0a38f1fe7ad7efd41a17f7babae4964dc
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-12 09:55:30 -0700

    Changed getting_solution to use code_database instead of creating random array

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,30 +9,30 @@ class Computer
   include GameNotifications
 
   attr_reader :computer_code, :current_game
-  attr_accessor :guess_database
+  attr_accessor :code_database
 
   def initialize(current_game)
     # @computer_code = generate_code
-    @guess_database = Array(1111..9999)
+    @code_database = Array(1111..9999)
     @current_game = current_game
   end
 
   def getting_solution
-    Array.new(4).map { rand(1..9).to_s }
+    code_database.sample.to_s.split('')
   end
 
   # Method to get rid of unviable combinations after each guess, based on the returning hint
   def reduce_guess_array(hint_array, guessed_combination)
-    guess_database.reduce([]) do |return_array, possible_combination|
+    code_database.reduce([]) do |return_array, possible_combination|
       if current_game.check_guess(possible_combination.to_s.split(''), guessed_combination) == hint_array
         return_array << possible_combination
         return_array
       end
-      @guess_database = return_array
+      @code_database = return_array
     end
   end
 
   def getting_guess
-    current_game.round.zero? ? guess_database[11].to_s.split('') : guess_database[0].to_s.split('')
+    current_game.round.zero? ? code_database[11].to_s.split('') : code_database[0].to_s.split('')
   end
 end
