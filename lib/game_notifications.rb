e627a6215d58ffffca2f2d666203d33e253dec18
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-06-12 12:07:56 -0700

    Minor change

--- lib/game_notifications.rb
+++ lib/game_notifications.rb
@@ -33,7 +33,7 @@ A true Mastermind!\n\n".colorize(:green)
   def text_hint(last_guess, hint)
     puts "             | A  | B  | C  | D  |
 Last Guess:  | #{last_guess[0]}  | #{last_guess[1]}  | #{last_guess[2]}  | #{last_guess[3]}  |
-Hint:       | #{hint[0]} | #{hint[1]} | #{hint[2]} | #{hint[3]} |\n\n".colorize(:blue)
+Hint:        | #{hint[0]} | #{hint[1]} | #{hint[2]} | #{hint[3]} |\n\n".colorize(:blue)
   end
 
   def text_type_guess
